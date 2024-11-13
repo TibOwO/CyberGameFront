21effc973adb1048b2c78df62500780c1ea8816d
Thibault Amettler <thibaultamettler@gmail.com>
2024-11-13 12:49:59 +0100

    Creation d'une page Register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component'; // Adaptez ce chemin si nécessaire
+import { RegisterComponent } from './auth/register/register.component'; // Adaptez ce chemin si nécessaire
 
 export const routes: Routes = [
-    { path: 'auth/login', component: LoginComponent }
+    { path: 'auth/login', component: LoginComponent },
+    { path: 'auth/register', component: RegisterComponent }
 ];
